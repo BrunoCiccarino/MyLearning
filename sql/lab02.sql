cd9d06701a6a7a89f17ea8b029f3f7870fa53fd0
Bruno Ciccarino λ <82242552+BrunoCiccarino@users.noreply.github.com>
2024-09-05 18:54:47 -0300

    Add files via upload

--- sql/lab02.sql
+++ sql/lab02.sql
@@ -1,12 +1,56 @@
-DROP DATABASE IF EXISTS cadastro;
-CREATE DATABASE cadastro;
+DROP DATABASE IF EXISTS Aulas_sql;
+CREATE DATABASE Aulas_sql;
 
-CREATE TABLE contato_1nf (
-	_id INT AUTO_INCREMENT PRIMARY KEY,
+USE Aulas_sql;
+CREATE TABLE contatos (
+	_id SERIAL PRIMARY KEY,
     nome VARCHAR(100),
     emails VARCHAR(255)
 );
-INSERT INTO contato (nome, emails) VALUES
-('João', 'joao@pudim.com.br, joao@bol.com.br, joao@ig.com.br'),
-('Maria', 'maria@maria.com.br, maria@gmail.com');
 
+INSERT INTO contatos (nome, emails) VALUES
+('joão', 'Joao@gmail.com, joao@pudim.com, joao@uber.com'),
+('Roberto', 'roberto@gmail.com, roberto@pudim.com, roberto@uber.com'),
+('Matheus', 'matheus@gmail.com, matheus@pudim.com, matheus@uber.com'),
+('Bruno', 'bruno@gmail.com, bruno@pudim.com, bruno@uber.com'),
+('Maria', 'Maria@gmail.com, maria@pudim.com');
+
+SELECT * FROM contatos;
+
+CREATE TABLE contato_1nf (
+	_id SERIAL PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE email (
+	_id SERIAL PRIMARY KEY,
+	contato_fk BIGINT UNSIGNED,
+    email VARCHAR(255) NOT NULL,
+    FOREIGN KEY (contato_fk) REFERENCES contato_1nf(_id)
+);
+
+INSERT INTO contato_1nf (nome) VALUES
+('João'),
+('Roberto'),
+('Matheus'),
+('Bruno'),
+('Maria');
+
+INSERT INTO email (contato_fk, email) VALUES 
+(1, 'Joao@gmail.com'),
+(1, 'joao@pudim.com'),
+(1, 'joao@uber.com'),
+(2, 'roberto@gmail.com'),
+(2, 'roberto@pudim.com'),
+(2, 'roberto@uber.com'),
+(3, 'matheus@gmail.com'),
+(3, 'matheus@pudim.com'),
+(3, 'matheus@uber.com'),
+(4, 'bruno@gmail.com'),
+(4, 'bruno@pudim.com'),
+(4, 'bruno@uber.com'),
+(5, 'Maria@gmail.com');
+
+SELECT c._id, c.nome, e.email
+	FROM contato_1nf AS c
+	JOIN email e ON c._id = e.contato_fk;
